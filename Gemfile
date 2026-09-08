5a91da4299c6450e53b35aaca16f8fbe8f68d5b3
syn <syn@syn.as>
2026-09-08 10:28:48 +0100

    ✨ feat: scaffold keyhaus cli project
    - Add Gemfile declaring dependencies on tty-prompt and commander
    - Add Gemfile.lock pinning resolved gem versions and checksums
    - Add main.rb entry point using commander to define the keyhaus
      program with stub serve, doctor, config, version, and help
      commands, each awaiting delegation to Keyhaus::Commands classes
    
    Establishes the initial project skeleton and dependency setup for
    the Ruby-based DNS server CLI tool before real implementation logic
    is added.

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# gem "rails"
+
+gem "tty-prompt", "~> 0.23.1"
+
+gem "commander", "~> 6.0"
---
+++ main.rb
@@ -0,0 +1,64 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'commander/import'
+
+program :name, 'keyhaus'
+program :version, '0.0.1'
+program :description, 'keyhaus dns server'
+
+command :serve do |c|
+  c.syntax = 'keyhaus serve [options]'
+  c.summary = ''
+  c.description = ''
+  c.example 'description', 'command example'
+  c.option '--some-switch', 'Some switch that does something'
+  c.action do |args, options|
+    # Do something or c.when_called Keyhaus::Commands::Serve
+  end
+end
+
+command :doctor do |c|
+  c.syntax = 'keyhaus doctor [options]'
+  c.summary = ''
+  c.description = ''
+  c.example 'description', 'command example'
+  c.option '--some-switch', 'Some switch that does something'
+  c.action do |args, options|
+    # Do something or c.when_called Keyhaus::Commands::Doctor
+  end
+end
+
+command :config do |c|
+  c.syntax = 'keyhaus config [options]'
+  c.summary = ''
+  c.description = ''
+  c.example 'description', 'command example'
+  c.option '--some-switch', 'Some switch that does something'
+  c.action do |args, options|
+    # Do something or c.when_called Keyhaus::Commands::Config
+  end
+end
+
+command :version do |c|
+  c.syntax = 'keyhaus version [options]'
+  c.summary = ''
+  c.description = ''
+  c.example 'description', 'command example'
+  c.option '--some-switch', 'Some switch that does something'
+  c.action do |args, options|
+    # Do something or c.when_called Keyhaus::Commands::Version
+  end
+end
+
+command :help do |c|
+  c.syntax = 'keyhaus help [options]'
+  c.summary = ''
+  c.description = ''
+  c.example 'description', 'command example'
+  c.option '--some-switch', 'Some switch that does something'
+  c.action do |args, options|
+    # Do something or c.when_called Keyhaus::Commands::Help
+  end
+end
+
